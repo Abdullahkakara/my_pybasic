210f188abd88a3d16989af71c0f8623a25300c3e
Abdullahkakara <mamunlotani@gmail.com>
2023-12-05 18:08:42 +0600

    added file

---
+++ mid1.py
@@ -0,0 +1,14 @@
+
+# a=int(input("Enter the value:"))
+# n=1
+# while n<=a:
+#     if n%2==0:
+#         print(n)
+#     n+=1
+
+# i = 2
+# while i<=20:
+#     print(i)
+#     i = i+2
+    
+print(f"this is my number {50}")
